d73db8a99d0d7823421b18970da2c60f2416ed13
beomsic <hhqjatjr@gmail.com>
2025-05-18 12:34:49 +0900

    feat: 정적 팩토리 메서드 추가 (of)

--- motimo-domain/src/main/java/kr/co/domain/user/User.java
+++ motimo-domain/src/main/java/kr/co/domain/user/User.java
@@ -1,4 +1,8 @@
 package kr.co.domain.user;
 
 public record User(Long id, String username, String password) {
+
+    public static User of(String username, String password) {
+        return new User(null, username, password);
+    }
 }

f2c1286fb4750eca1ca56839eec357c9c49a0183
beomsic <hhqjatjr@gmail.com>
2025-05-19 01:20:17 +0900

    feat: Add CustomUserDetail and CustomUserDetailsService
    - UserPrincipal 클래스 추가하여 UserDetails 인터페이스 구현
    - CustomUserDetailsService에서 UserRepository 기반 사용자 조회 구현
    - Spring Security 인증 로직에서 사용자 정보를 UserPrincipal 기반으로 처리 가능하도록 통합

---
+++ motimo-api/src/main/java/kr/co/api/security/UserPrincipal.java
@@ -0,0 +1,52 @@
+package kr.co.api.security;
+
+import kr.co.domain.user.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(Long id, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrincipal of(User user) {
+        List<GrantedAuthority> authorities = Collections.
+                singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UserPrincipal(
+                user.id(),
+                user.email(),
+                user.password(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}

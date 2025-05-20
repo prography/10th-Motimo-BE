9fd2f6299613b09dba627c709574455d59f371e4
beomsic <hhqjatjr@gmail.com>
2025-05-20 13:47:45 +0900

    remove: 사용하지 않는 클래스 삭제

--- motimo-domain/src/main/java/kr/co/domain/user/dto/UserInfo.java
+++
@@ -1,9 +0,0 @@
-package kr.co.domain.user.dto;
-
-import kr.co.domain.user.model.User;
-
-public record UserInfo(Long id, String email, String nickname) {
-    public static UserInfo from(User user) {
-        return new UserInfo(user.getId(), user.getEmail(), user.getNickname());
-    }
-}

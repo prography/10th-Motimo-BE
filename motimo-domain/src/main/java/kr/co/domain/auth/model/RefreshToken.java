4db4103d4c73d62b2385a22d88a52d436ae68841
beomsic <hhqjatjr@gmail.com>
2025-05-23 02:08:40 +0900

    feat: @NonNull

--- motimo-domain/src/main/java/kr/co/domain/auth/model/RefreshToken.java
+++ motimo-domain/src/main/java/kr/co/domain/auth/model/RefreshToken.java
@@ -3,6 +3,7 @@ package kr.co.domain.auth.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.util.UUID;
 
@@ -12,7 +13,10 @@ import java.util.UUID;
 public class RefreshToken {
     @Builder.Default
     private UUID id = null;
+    @NonNull
     private UUID tokenId;
+    @NonNull
     private UUID userId;
+    @NonNull
     private String token;
 }

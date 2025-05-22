29ee195578eef52608e7b66a99d387b176edab43
beomsic <hhqjatjr@gmail.com>
2025-05-22 14:03:32 +0900

    feat: auth custom exception

---
+++ motimo-domain/src/main/java/kr/co/domain/auth/exception/TokenMismatchException.java
@@ -0,0 +1,10 @@
+package kr.co.domain.auth.exception;
+
+import kr.co.domain.common.exception.BusinessException;
+
+public class TokenMismatchException extends BusinessException {
+    public TokenMismatchException() {
+        super(AuthErrorCode.TOKEN_MISMATCH);
+    }
+}
+

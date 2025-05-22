8493fe162075b1878c6044d1a1157d60ea109f52
beomsic <hhqjatjr@gmail.com>
2025-05-22 14:09:36 +0900

    feat: @AuthToken

--- motimo-api/src/main/java/kr/co/api/config/WebConfig.java
+++ motimo-api/src/main/java/kr/co/api/config/WebConfig.java
@@ -1,6 +1,8 @@
 package kr.co.api.config;
 
+import kr.co.api.security.resolver.AuthTokenArgumentResolver;
 import kr.co.api.security.resolver.AuthUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,16 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthUserArgumentResolver authUserArgumentResolver;
-
-    public WebConfig(AuthUserArgumentResolver authUserArgumentResolver) {
-        this.authUserArgumentResolver = authUserArgumentResolver;
-    }
+    private final AuthTokenArgumentResolver authTokenArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
+        resolvers.add(authTokenArgumentResolver);
     }
 }

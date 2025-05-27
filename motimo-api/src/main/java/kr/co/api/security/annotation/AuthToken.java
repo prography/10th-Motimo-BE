4331bb7da5ef3b1d5f349c55722e3112a5508354
jongeuni <lje0818ovo@naver.com>
2025-05-27 18:15:44 +0900

    #18 docs: 사용자 정보 애노테이션 스웨거에 노출 안 되도록 추가

--- motimo-api/src/main/java/kr/co/api/security/annotation/AuthToken.java
+++ motimo-api/src/main/java/kr/co/api/security/annotation/AuthToken.java
@@ -1,9 +1,12 @@
 package kr.co.api.security.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Parameter(hidden = true)
 public @interface AuthToken {
 }

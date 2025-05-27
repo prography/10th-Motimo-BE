4f035d6f69bd47087f55a23dfdb755f4e7dd1e13
jongeuni <lje0818ovo@naver.com>
2025-05-27 18:37:04 +0900

    #18 docs: 사용자 가입일 조회 path 추가

--- motimo-api/src/main/java/kr/co/api/user/controller/UserController.java
+++ motimo-api/src/main/java/kr/co/api/user/controller/UserController.java
@@ -2,6 +2,7 @@ package kr.co.api.user.controller;
 
 import kr.co.api.user.controller.rqrs.UserJoinedDateRs;
 import kr.co.api.user.docs.UserControllerSwagger;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/users")
 public class UserController implements UserControllerSwagger {
 
+    @GetMapping("/joined-dates")
     public UserJoinedDateRs readJoinedDate() {
         return null;
     }

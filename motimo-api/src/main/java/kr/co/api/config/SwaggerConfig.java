d1d13015e85be4b3a2e8a1934776695064e05214
jongeuni <lje0818ovo@naver.com>
2025-05-27 18:10:12 +0900

    #18 docs: swagger에 token 설정

--- motimo-api/src/main/java/kr/co/api/config/SwaggerConfig.java
+++ motimo-api/src/main/java/kr/co/api/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package kr.co.api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,39 +14,51 @@ public class SwaggerConfig {
     @Bean
     public GroupedOpenApi motimoAPI() {
         return GroupedOpenApi
-                .builder()
-                .group("Motimo API List")
-                .addOpenApiCustomizer(
-                        openApi ->
-                                openApi
-                                        .setInfo(
-                                                new Info()
-                                                        .title("Motimo API List") // API 제목
-                                                        .description("모티모 API 목록입니다.") // API 설명
-                                                        .version("1.0.0") // API 버전
-                                        )
-                )
-                .build();
+            .builder()
+            .group("Motimo API List")
+            .addOpenApiCustomizer(openApi -> {
+                jwtSecuritySetting(openApi);
+                openApi.setInfo(
+                    new Info()
+                        .title("Motimo API List")
+                        .description("모티모 API 목록입니다.")
+                        .version("1.0.0")
+                );
+            })
+            .build();
     }
+
+
 
     @Bean
     public GroupedOpenApi version1() {
         return GroupedOpenApi
-                .builder()
-                .group("Motimo V1 API List")
-                .pathsToMatch("/v1/**")
-                .addOpenApiCustomizer(
-                        openApi ->
-                                openApi
-                                        .setInfo(
-                                                new Info()
-                                                        .title("Motimo Version1 API List")
-                                                        .description("모티모 버전 1 API 목록입니다.")
-                                                        .version("1.0.0")
-                                        )
-                )
-                .build();
+            .builder()
+            .group("Motimo V1 API List")
+            .pathsToMatch("/v1/**")
+            .addOpenApiCustomizer(openApi -> {
+                jwtSecuritySetting(openApi);
+                openApi.setInfo(
+                    new Info()
+                        .title("Motimo Version1 API List")
+                        .description("모티모 버전 1 API 목록입니다.")
+                        .version("1.0.0")
+                );
+            })
+            .build();
     }
 
+
+    private void jwtSecuritySetting(OpenAPI openApi) {
+        openApi.getComponents().addSecuritySchemes("BearerAuth",
+            new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+        );
+
+        openApi.addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+    }
 
 }

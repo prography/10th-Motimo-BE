9db7e45d74e139cb3bec730fd015982d42ef2a33
jongeuni <lje0818ovo@naver.com>
2025-05-29 00:25:48 +0900

    #21 fix: Phrase -> CheerPhrase로 명확화

--- motimo-api/src/main/java/kr/co/api/cheer/CheerController.java
+++ motimo-api/src/main/java/kr/co/api/cheer/CheerController.java
@@ -1,7 +1,7 @@
 package kr.co.api.cheer;
 
 import kr.co.api.cheer.docs.CheerControllerSwagger;
-import kr.co.api.cheer.rqrs.PhraseReadRs;
+import kr.co.api.cheer.rqrs.CheerPhraseRs;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheerController implements CheerControllerSwagger {
 
     @GetMapping
-    public PhraseReadRs readPhrase() {
+    public CheerPhraseRs readCheerPhrase() {
         return null;
     }
 }
--- motimo-api/src/main/java/kr/co/api/cheer/rqrs/CheerPhraseRs.java
+++ motimo-api/src/main/java/kr/co/api/cheer/rqrs/CheerPhraseRs.java
@@ -2,8 +2,8 @@ package kr.co.api.cheer.rqrs;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record PhraseReadRs(
+public record CheerPhraseRs(
     @Schema(description = "응원 문구", example = "목표는 멀어도 나는 계속 가는중")
-    String phrase
+    String cheerPhrase
 ) {
 }

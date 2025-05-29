6f1362eb59592214582ba7c092151f95de340d97
jongeuni <lje0818ovo@naver.com>
2025-05-29 13:14:27 +0900

    delete: 사용자 가입일 조회 API 삭제

--- motimo-api/src/main/java/kr/co/api/user/controller/UserController.java
+++ motimo-api/src/main/java/kr/co/api/user/controller/UserController.java
@@ -1,21 +1,11 @@
 package kr.co.api.user.controller;
 
-import kr.co.api.user.controller.rqrs.UserJoinedDateRs;
 import kr.co.api.user.docs.UserControllerSwagger;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController implements UserControllerSwagger {
 
-    @GetMapping("/me/joined-date")
-    public UserJoinedDateRs readJoinedDate() {
-        LocalDate joinedDate = LocalDate.of(2025, 5, 20);
-        return new UserJoinedDateRs(joinedDate, (int) ChronoUnit.DAYS.between(joinedDate, LocalDate.now()));
-    }
 }
--- motimo-api/src/main/java/kr/co/api/user/docs/UserControllerSwagger.java
+++ motimo-api/src/main/java/kr/co/api/user/docs/UserControllerSwagger.java
@@ -1,11 +1,7 @@
 package kr.co.api.user.docs;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kr.co.api.user.controller.rqrs.UserJoinedDateRs;
 
 @Tag(name = "사용자 API", description = "사용자 관련 API 목록입니다")
 public interface UserControllerSwagger {
-    @Operation(summary = "사용자 가입일 조회 API", description = "사용자 가입일을 조회합니다.")
-    UserJoinedDateRs readJoinedDate();
 }

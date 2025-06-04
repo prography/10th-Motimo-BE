361c0d64c618c20ee8573601443411786a848786
beomsic <hhqjatjr@gmail.com>
2025-06-04 17:19:15 +0900

    refactor: 메서드 네이밍 수정

--- motimo-api/src/main/java/kr/co/api/todo/service/TodoCommandService.java
+++ motimo-api/src/main/java/kr/co/api/todo/service/TodoCommandService.java
@@ -3,11 +3,12 @@ package kr.co.api.todo.service;
 import java.time.LocalDate;
 import java.util.UUID;
 import kr.co.domain.common.event.Events;
-import kr.co.domain.common.event.ImageDeletedEvent;
+import kr.co.domain.common.event.FileDeletedEvent;
 import kr.co.domain.todo.Emotion;
 import kr.co.domain.todo.Todo;
 import kr.co.domain.todo.TodoResult;
 import kr.co.domain.todo.repository.TodoRepository;
+import kr.co.domain.todo.repository.TodoResultRepository;
 import kr.co.infra.storage.service.StorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class TodoCommandService {
 
     private final TodoRepository todoRepository;
+    private final TodoResultRepository todoResultRepository;
     private final StorageService storageService;
 
     public void createTodo(UUID userId, UUID subGoalId, String title, LocalDate date) {
@@ -32,39 +34,39 @@ public class TodoCommandService {
         todoRepository.save(todo);
     }
 
-    public void submitTodoResult(UUID userId, UUID id, Emotion emotion, String content,
-            MultipartFile image) {
-        Todo todo = todoRepository.findById(id);
+    public void submitTodoResult(UUID userId, UUID todoId, Emotion emotion, String content,
+            MultipartFile file) {
+        Todo todo = todoRepository.findById(todoId);
         todo.validateAuthor(userId);
 
-        String imageName = "";
-        if (image != null && !image.isEmpty()) {
-            imageName = String.format("todo/%s/%s", id, UUID.randomUUID());
-            storageService.uploadImage(image, imageName);
+        String fileUrl = "";
+        if (file != null && !file.isEmpty()) {
+            String filename = String.format("todo/%s/%s", todoId, UUID.randomUUID());
+            fileUrl = storageService.store(file, filename);
             // 이미지 삭제 이벤트 발행 (트랜잭션 롤백 시 동작)
-            Events.publishEvent(new ImageDeletedEvent(imageName));
+            Events.publishEvent(new FileDeletedEvent(fileUrl));
         }
 
-        TodoResult result = TodoResult.builder()
+        TodoResult record = TodoResult.builder()
+                .todoId(todoId)
                 .emotion(emotion)
                 .content(content)
-                .imageName(imageName)
+                .fileUrl(fileUrl)
                 .build();
 
-        todo.complete(result);
+        todoResultRepository.save(record);
+    }
+
+    public void toggleTodoCompletion(UUID userId, UUID todoId) {
+        Todo todo = todoRepository.findById(todoId);
+        todo.validateAuthor(userId);
+        todo.toggleCompletion();
         todoRepository.save(todo);
     }
 
-    public void cancelTodoResult(UUID userId, UUID id) {
-        Todo todo = todoRepository.findById(id);
+    public void deleteById(UUID userId, UUID todoId) {
+        Todo todo = todoRepository.findById(todoId);
         todo.validateAuthor(userId);
-        todo.cancelCompletion();
-        todoRepository.save(todo);
-    }
-
-    public void deleteById(UUID userId, UUID id) {
-        Todo todo = todoRepository.findById(id);
-        todo.validateAuthor(userId);
-        todoRepository.deleteById(id);
+        todoRepository.deleteById(todoId);
     }
 }

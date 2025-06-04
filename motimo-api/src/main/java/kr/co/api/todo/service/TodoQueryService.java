49f7e5cbca7e69e06fd24e97b69ecc453d1ec61b
beomsic <hhqjatjr@gmail.com>
2025-06-04 17:18:52 +0900

    refactor: 반환타입 변경

--- motimo-api/src/main/java/kr/co/api/todo/service/TodoQueryService.java
+++ motimo-api/src/main/java/kr/co/api/todo/service/TodoQueryService.java
@@ -1,13 +1,15 @@
 package kr.co.api.todo.service;
 
+import java.util.Optional;
+import java.util.UUID;
 import kr.co.domain.common.pagination.CustomSlice;
-import kr.co.domain.todo.Todo;
+import kr.co.domain.todo.TodoResult;
+import kr.co.domain.todo.dto.TodoSummary;
 import kr.co.domain.todo.repository.TodoRepository;
+import kr.co.domain.todo.repository.TodoResultRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,16 +17,17 @@ import java.util.UUID;
 public class TodoQueryService {
 
     private final TodoRepository todoRepository;
-
-    public Todo getTodo(UUID id) {
-        return todoRepository.findById(id);
-    }
-
-    public CustomSlice<Todo> getTodosBySubGoal(UUID subGoalId, int page, int size) {
+    private final TodoResultRepository todoResultRepository;
+    
+    public CustomSlice<TodoSummary> getTodosBySubGoal(UUID subGoalId, int page, int size) {
         return todoRepository.findAllBySubGoalId(subGoalId, page, size);
     }
 
-    public CustomSlice<Todo> getTodosByUser(UUID userId, int page, int size) {
+    public CustomSlice<TodoSummary> getTodosByUser(UUID userId, int page, int size) {
         return todoRepository.findAllByUserId(userId, page, size);
+    }
+
+    public Optional<TodoResult> getTodoResultByTodoId(UUID todoId) {
+        return todoResultRepository.findByTodoId(todoId);
     }
 }

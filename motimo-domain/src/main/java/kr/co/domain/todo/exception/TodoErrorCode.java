2619a8fd5e0cf955049ea89cd66e417f71067d4c
beomsic <hhqjatjr@gmail.com>
2025-06-04 17:15:07 +0900

    feat: todo result가 없는 경우 커스텀 예외 추가

--- motimo-domain/src/main/java/kr/co/domain/todo/exception/TodoErrorCode.java
+++ motimo-domain/src/main/java/kr/co/domain/todo/exception/TodoErrorCode.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TodoErrorCode implements ErrorCode {
     TODO_NOT_FOUND("todo를 찾을 수 없습니다.", 404),
+    TODO_RESULT_NOT_SUBMITTED("투두 결과가 아직 제출되지 않았습니다.", 404),
     TODO_ACCESS_DENIED("해당 TODO에 대한 권한이 없습니다.", 403),
     ;
 

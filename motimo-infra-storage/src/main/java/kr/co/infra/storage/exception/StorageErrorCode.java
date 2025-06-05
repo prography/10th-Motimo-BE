62a8ff8d8300bbe4f2fc49ce2546e71e1f8678e3
beomsic <hhqjatjr@gmail.com>
2025-06-06 02:36:15 +0900

    feat: 파일이 없거나 비어있는 경우 예외 추가

--- motimo-infra-storage/src/main/java/kr/co/infra/storage/exception/StorageErrorCode.java
+++ motimo-infra-storage/src/main/java/kr/co/infra/storage/exception/StorageErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum StorageErrorCode implements ErrorCode {
     FILE_UPLOAD_FAILED("파일 업로드에 실패했습니다.", 500),
-    FILE_DELETE_FAILED("파일 삭제에 실패했습니다.", 500);
+    FILE_DELETE_FAILED("파일 삭제에 실패했습니다.", 500),
+    INVALID_FILE("파일이 없거나 비어 있습니다.", 400);
 
     private final String message;
     private final int statusCode;

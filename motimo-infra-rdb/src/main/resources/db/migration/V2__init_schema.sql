8030bb00bf4aae1ff4b7e6ca166d1c6b368114a1
jongeuni <lje0818ovo@naver.com>
2025-06-12 16:20:07 +0900

    #29 add: 목표 테이블 ddl 추가

--- motimo-infra-rdb/src/main/resources/db/migration/V2__init_schema.sql
+++ motimo-infra-rdb/src/main/resources/db/migration/V2__init_schema.sql
@@ -54,3 +54,31 @@ CREATE TABLE todo_result
     is_deleted BOOLEAN   DEFAULT FALSE,
     FOREIGN KEY (todo_id) REFERENCES todo (id)
 );
+
+CREATE TABLE goals
+(
+    id           UUID  PRIMARY KEY,
+    user_id      UUID,
+    title        VARCHAR(255),
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    completed    BOOLEAN NOT NULL,
+    completed_at TIMESTAMP,
+    month        INTEGER,
+    due_date     date,
+    is_deleted   BOOLEAN NOT NULL
+);
+
+CREATE TABLE sub_goals
+(
+    id                   UUID  PRIMARY KEY,
+    goal_id              UUID,
+    title                VARCHAR(255),
+    importance           INTEGER NOT NULL,
+    completed            BOOLEAN NOT NULL,
+    completed_changed_at TIMESTAMP WITHOUT TIME ZONE,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_deleted           BOOLEAN NOT NULL,
+    FOREIGN KEY (goal_id) REFERENCES goals (id)
+);

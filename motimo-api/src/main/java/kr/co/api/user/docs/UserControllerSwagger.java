192dbfeddc29596ca19dcf56ae3964de7607cfaa
beomsic <hhqjatjr@gmail.com>
2025-06-13 16:54:46 +0900

    refactor: 유저의 투두리스트 조회 페이지네이션 제거

--- motimo-api/src/main/java/kr/co/api/user/docs/UserControllerSwagger.java
+++ motimo-api/src/main/java/kr/co/api/user/docs/UserControllerSwagger.java
@@ -1,16 +1,15 @@
 package kr.co.api.user.docs;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import java.util.UUID;
 import kr.co.api.todo.rqrs.TodoRs;
 import kr.co.domain.common.pagination.CustomSlice;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "사용자 API", description = "사용자 관련 API 목록입니다")
 public interface UserControllerSwagger {
@@ -21,12 +20,6 @@ public interface UserControllerSwagger {
             @ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
             @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자")
     })
-    CustomSlice<TodoRs> getMyTodos(
-            UUID userId,
-            @Parameter(description = "페이지 번호", required = true, example = "0")
-            @RequestParam("page") int page,
-            @Parameter(description = "페이지 크기", required = true, example = "10")
-            @RequestParam("size") int size
-    );
+    List<TodoRs> getMyTodos(UUID userId);
 
 }

c06e8b04c0eecc56accd2ccde67c8f52ade6234b
beomsic <hhqjatjr@gmail.com>
2025-06-19 14:51:33 +0900

    refactor: 리스너 클래스 네이밍 수정

--- motimo-api/src/main/java/kr/co/api/event/listener/FileEventListener.java
+++ motimo-api/src/main/java/kr/co/api/event/listener/FileEventListener.java
@@ -1,6 +1,7 @@
 package kr.co.api.event.listener;
 
 import kr.co.domain.common.event.FileDeletedEvent;
+import kr.co.domain.common.event.FileRollbackEvent;
 import kr.co.infra.storage.service.StorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,18 +12,28 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class FileDeleteEventListener {
+public class FileEventListener {
 
     private final StorageService storageService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
-    public void handleFileDeletedOnRollback(FileDeletedEvent event) {
+    public void handleFileRollback(FileRollbackEvent event) {
         String filePath = event.getFilePath();
         try {
             storageService.delete(filePath);
         } catch (Exception e) {
             // todo: 삭제 실패 시 로깅만, 재시도 x => 추후 아웃박스 도입 고려)
             log.error("롤백 후 파일 삭제 실패 {}:", e.getMessage());
+        }
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleFileDelete(FileDeletedEvent event) {
+        String filePath = event.getFilePath();
+        try {
+            storageService.delete(filePath);
+        } catch (Exception e) {
+            log.error("커밋 후 파일 삭제 실패 {}: {}", filePath, e.getMessage());
         }
     }
 

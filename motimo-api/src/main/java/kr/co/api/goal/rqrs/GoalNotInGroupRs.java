922cbd6464f509f5d3f8681e2304227b9ed2710f
jongeuni <lje0818ovo@naver.com>
2025-07-01 23:59:11 +0900

    #55 add: 참여 대기중인 목표 rs 수정

--- motimo-api/src/main/java/kr/co/api/goal/rqrs/GoalNotInGroupRs.java
+++ motimo-api/src/main/java/kr/co/api/goal/rqrs/GoalNotInGroupRs.java
@@ -1,8 +1,11 @@
 package kr.co.api.goal.rqrs;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
 
 public record GoalNotInGroupRs(
+        @Schema(description = "목표 아이디")
+        UUID id,
         @Schema(description = "목표 제목")
         String title
 ) {

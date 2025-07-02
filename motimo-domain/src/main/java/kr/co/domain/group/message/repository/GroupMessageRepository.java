196f05f2a86c12b0e1e52d9823ba5acb3b90ce2d
beomsic <hhqjatjr@gmail.com>
2025-07-02 18:56:45 +0900

    feat: 그룹 메시지 repository

---
+++ motimo-domain/src/main/java/kr/co/domain/group/message/repository/GroupMessageRepository.java
@@ -0,0 +1,8 @@
+package kr.co.domain.group.message.repository;
+
+import kr.co.domain.group.message.GroupMessage;
+
+public interface GroupMessageRepository {
+
+    GroupMessage create(GroupMessage groupMessage);
+}

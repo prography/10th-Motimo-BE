89b88241ee1230c4611586275c0e3870b86d4c97
beomsic <hhqjatjr@gmail.com>
2025-07-03 20:29:35 +0900

    feat: 리액션타입추가

--- motimo-domain/src/main/java/kr/co/domain/group/reaction/ReactionType.java
+++ motimo-domain/src/main/java/kr/co/domain/group/reaction/ReactionType.java
@@ -1,4 +1,19 @@
 package kr.co.domain.group.reaction;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ReactionType {
+    GOOD("굿"),
+    COOL("멋져"),
+    CHEER_UP("힘내"),
+    BEST("최고"),
+    LIKE("좋아요");
+
+    private final String description;
+
+    public String getDescription() {
+        return description;
+    }
 }

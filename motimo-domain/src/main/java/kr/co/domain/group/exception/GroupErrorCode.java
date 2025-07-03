6923aec7bbe905ef6dba6c1a3879b8b0fb1e4702
beomsic <hhqjatjr@gmail.com>
2025-07-04 00:09:47 +0900

    remove: ; 제거

--- motimo-domain/src/main/java/kr/co/domain/group/exception/GroupErrorCode.java
+++ motimo-domain/src/main/java/kr/co/domain/group/exception/GroupErrorCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum GroupErrorCode implements ErrorCode {
     GROUP_NOT_FOUND("group를 찾을 수 없습니다.", 404),
-    UNSUPPORTED_MESSAGE_TYPE("지원하지 않는 메시지 타입입니다.", 400);;
+    UNSUPPORTED_MESSAGE_TYPE("지원하지 않는 메시지 타입입니다.", 400);
 
     private final String message;
     private final int statusCode;

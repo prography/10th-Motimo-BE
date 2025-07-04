4eb169821e3a56e0951462aa56e49fb2a6529e07
beomsic <hhqjatjr@gmail.com>
2025-07-04 17:36:10 +0900

    feat: 유저의 아이디들을 통해 해당 유저들의 정보 조회

--- motimo-domain/src/main/java/kr/co/domain/user/repository/UserRepository.java
+++ motimo-domain/src/main/java/kr/co/domain/user/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package kr.co.domain.user.repository;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import kr.co.domain.user.model.ProviderType;
 import kr.co.domain.user.model.User;
-
-import java.util.UUID;
 
 public interface UserRepository {
 
@@ -16,4 +17,6 @@ public interface UserRepository {
     User save(User user);
 
     boolean existsByEmailAndProviderType(String email, ProviderType providerType);
+
+    List<User> findAllByIdsIn(Set<UUID> userIds);
 }

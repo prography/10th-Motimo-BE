89ac340756c071ce9565d87f4cfe287352b051d3
beomsic <hhqjatjr@gmail.com>
2025-07-06 14:28:15 +0900

    feat: 메시지 로드 실패 예외 추가

--- motimo-domain/src/main/java/kr/co/domain/group/exception/GroupErrorCode.java
+++ motimo-domain/src/main/java/kr/co/domain/group/exception/GroupErrorCode.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum GroupErrorCode implements ErrorCode {
     GROUP_NOT_FOUND("group를 찾을 수 없습니다.", 404),
+    MESSAGE_LOAD_FAIL("메시지를 로드하는데 실패했습니다.", 500),
     UNSUPPORTED_MESSAGE_TYPE("지원하지 않는 메시지 타입입니다.", 400);
 
     private final String message;

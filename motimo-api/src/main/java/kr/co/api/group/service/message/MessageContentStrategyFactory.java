73a70895f0f9090aa7bc70f01404e9a057bd6c5a
beomsic <hhqjatjr@gmail.com>
2025-07-06 14:21:38 +0900

    feat: 메시지 타입이 중복된 경우 고려해 코드 수정

--- motimo-api/src/main/java/kr/co/api/group/service/message/MessageContentStrategyFactory.java
+++ motimo-api/src/main/java/kr/co/api/group/service/message/MessageContentStrategyFactory.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 import kr.co.domain.group.exception.UnsupportedGroupMessageTypeException;
 import kr.co.domain.group.message.GroupMessageType;
 import kr.co.domain.group.message.strategy.MessageContentStrategy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MessageContentStrategyFactory {
 
@@ -17,7 +19,12 @@ public class MessageContentStrategyFactory {
         this.strategies = strategies.stream()
                 .collect(Collectors.toMap(
                         MessageContentStrategy::getMessageType,
-                        strategy -> strategy
+                        strategy -> strategy,
+                        (existing, replacement) -> {
+                            log.warn("메시지 타입이 중복되었습니다: {}. 이미 사용중인 전략을 사용합니다.",
+                                    existing.getMessageType());
+                            return existing;
+                        }
                 ));
     }
 

f37e867b99572549b7a3d0da613af9cf9db95c1c
beomsic <hhqjatjr@gmail.com>
2025-07-06 14:28:31 +0900

    feat: 메시지 로드 중 예외를 처리하기 위한 try-catch 추가

--- motimo-api/src/main/java/kr/co/api/group/service/message/MessageContentLoader.java
+++ motimo-api/src/main/java/kr/co/api/group/service/message/MessageContentLoader.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import kr.co.domain.group.exception.MessageContentLoadingException;
 import kr.co.domain.group.message.GroupMessage;
 import kr.co.domain.group.message.MessageReference;
 import kr.co.domain.group.message.MessageReferenceType;
@@ -17,8 +18,11 @@ import kr.co.domain.todo.repository.TodoResultRepository;
 import kr.co.domain.user.model.User;
 import kr.co.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MessageContentLoader {
@@ -28,35 +32,44 @@ public class MessageContentLoader {
     private final UserRepository userRepository;
 
     public MessageContentData loadMessageContentData(List<GroupMessage> messages) {
-        EnumMap<MessageReferenceType, Set<UUID>> referenceTypeIds =
-                new EnumMap<>(MessageReferenceType.class);
 
-        referenceTypeIds.put(MessageReferenceType.TODO, new HashSet<>());
-        referenceTypeIds.put(MessageReferenceType.TODO_RESULT, new HashSet<>());
+        try {
+            EnumMap<MessageReferenceType, Set<UUID>> referenceTypeIds =
+                    new EnumMap<>(MessageReferenceType.class);
 
-        // 메시지 타입별로 필요한 ID들 추출
-        Set<UUID> userIds = messages.stream()
-                .map(GroupMessage::getUserId)
-                .collect(Collectors.toSet());
+            referenceTypeIds.put(MessageReferenceType.TODO, new HashSet<>());
+            referenceTypeIds.put(MessageReferenceType.TODO_RESULT, new HashSet<>());
 
-        for (GroupMessage m : messages) {
-            MessageReference msgReference = m.getMessageReference();
-            if (msgReference != null) {
-                referenceTypeIds.get(msgReference.messageReferenceType())
-                        .add(msgReference.referenceId());
+            // 메시지 타입별로 필요한 ID들 추출
+            Set<UUID> userIds = messages.stream()
+                    .map(GroupMessage::getUserId)
+                    .collect(Collectors.toSet());
+
+            for (GroupMessage m : messages) {
+                MessageReference msgReference = m.getMessageReference();
+                if (msgReference != null) {
+                    referenceTypeIds.get(msgReference.messageReferenceType())
+                            .add(msgReference.referenceId());
+                }
             }
+
+            // 배치 조회 실행
+            Map<UUID, TodoResult> todoResultMap = loadTodoResults(
+                    referenceTypeIds.get(MessageReferenceType.TODO_RESULT));
+
+            Map<UUID, Todo> todoMap = loadTodos(
+                    referenceTypeIds.get(MessageReferenceType.TODO), todoResultMap);
+
+            Map<UUID, User> userMap = loadUsers(userIds);
+
+            return new MessageContentData(todoMap, todoResultMap, userMap);
+        } catch (DataAccessException e) {
+            log.error("메시지 content 로드중 Database 에러가 발생했습니다 - {} messages", messages.size(), e);
+            throw new MessageContentLoadingException();
+        } catch (Exception e) {
+            log.error("메시지 content 로드 중 예상하지 못한 에러가 발생했습니다 - {} messages", messages.size(), e);
+            throw new MessageContentLoadingException();
         }
-
-        // 배치 조회 실행
-        Map<UUID, TodoResult> todoResultMap = loadTodoResults(
-                referenceTypeIds.get(MessageReferenceType.TODO_RESULT));
-
-        Map<UUID, Todo> todoMap = loadTodos(
-                referenceTypeIds.get(MessageReferenceType.TODO), todoResultMap);
-
-        Map<UUID, User> userMap = loadUsers(userIds);
-
-        return new MessageContentData(todoMap, todoResultMap, userMap);
     }
 
     private Map<UUID, Todo> loadTodos(Set<UUID> todoIds, Map<UUID, TodoResult> todoResults) {

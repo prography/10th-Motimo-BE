9faea4092bd34b2eb7c0ba4008374108e7d34bf6
beomsic <hhqjatjr@gmail.com>
2025-07-07 16:29:49 +0900

    refactor: 트랜잭션 전파값 수정

--- motimo-api/src/main/java/kr/co/api/group/service/GroupMessageCommandService.java
+++ motimo-api/src/main/java/kr/co/api/group/service/GroupMessageCommandService.java
@@ -5,7 +5,6 @@ import kr.co.domain.group.message.GroupMessage;
 import kr.co.domain.group.message.repository.GroupMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,12 +14,10 @@ public class GroupMessageCommandService {
 
     private final GroupMessageRepository groupMessageRepository;
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void createGroupMessage(GroupMessage groupMessage) {
         groupMessageRepository.create(groupMessage);
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void deleteAllByReferenceId(UUID referenceId) {
         groupMessageRepository.deleteAllByReferenceId(referenceId);
     }

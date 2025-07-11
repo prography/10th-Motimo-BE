cf60e56d5a63685bc32f925dca846fee59106494
beomsic <hhqjatjr@gmail.com>
2025-07-11 12:45:02 +0900

    feat: 유저 한줄 소개 필드 추가

--- motimo-api/src/main/java/kr/co/api/user/rqrs/UserRs.java
+++ motimo-api/src/main/java/kr/co/api/user/rqrs/UserRs.java
@@ -3,7 +3,7 @@ package kr.co.api.user.rqrs;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.UUID;
-import kr.co.domain.user.model.User;
+import kr.co.api.user.dto.UserProfileDto;
 
 public record UserRs(
         @Schema(description = "유저 Id", example = "0197157f-aea4-77bb-8581-3213eb5bd2ae")
@@ -12,13 +12,15 @@ public record UserRs(
         String email,
         @Schema(description = "유저 닉네임")
         String nickname,
+        @Schema(description = "유저 한줄 소개")
+        String bio,
         @Schema(description = "유저 프로필 이미지 url")
         String profileImageUrl,
         @Schema(description = "유저 생성 날짜", type = "date")
         LocalDateTime createdAt) {
 
-    public static UserRs from(User user) {
-        return new UserRs(user.getId(), user.getEmail(), user.getNickname(),
-                user.getProfileImageUrl(), user.getCreatedAt());
+    public static UserRs from(UserProfileDto dto) {
+        return new UserRs(dto.id(), dto.email(), dto.username(), dto.bio(), dto.profileUrl(),
+                dto.createdAt());
     }
 }

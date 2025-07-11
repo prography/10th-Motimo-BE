7f21372f0e5731b9ddd2ffdecb5cb70f0dbd8fbf
beomsic <hhqjatjr@gmail.com>
2025-07-11 16:07:26 +0900

    refactor: 하나로 sql 문 합치기

--- motimo-infra-rdb/src/main/resources/db/migration/V12__update_user_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-    ADD COLUMN bio VARCHAR(255);
--- motimo-infra-rdb/src/main/resources/db/migration/V13__update_user_table.sql
+++ motimo-infra-rdb/src/main/resources/db/migration/V13__update_user_table.sql
@@ -1,2 +1,5 @@
 ALTER TABLE users
+    ADD COLUMN bio VARCHAR(255);
+
+ALTER TABLE users
     RENAME COLUMN profile_image_url TO profile_image_path;

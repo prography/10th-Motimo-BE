43a1006929e09dea893f64824fada51775b74028
jongeuni <lje0818ovo@naver.com>
2025-07-23 18:42:30 +0900

    change: GroupMemberRepository 사용하는 부분 변경

--- motimo-infra-rdb/src/main/java/kr/co/infra/rdb/group/util/GroupMapper.java
+++ motimo-infra-rdb/src/main/java/kr/co/infra/rdb/group/util/GroupMapper.java
@@ -1,8 +1,10 @@
 package kr.co.infra.rdb.group.util;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import kr.co.domain.group.Group;
+import kr.co.domain.group.GroupMember;
 import kr.co.infra.rdb.group.entity.GroupEntity;
 import lombok.experimental.UtilityClass;
 
@@ -13,6 +15,14 @@ public class GroupMapper {
         return Group.builder()
                 .id(entity.getId())
                 .finishedDate(entity.getFinishedDate())
+                .build();
+    }
+
+    public static Group toDomain(GroupEntity entity, List<GroupMember> members) {
+        return Group.builder()
+                .id(entity.getId())
+                .finishedDate(entity.getFinishedDate())
+                .members(members)
                 .build();
     }
 
